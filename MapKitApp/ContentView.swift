5acef1277f61dbc545b92394b37983a6a365cb03
富澤明大 <akihirowwzai04a@gmail.com>
2024-08-04 11:10:10 +0900

    Initial Commit

---
+++ MapKitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitApp
+//
+//  Created by 富澤明大 on 2024/08/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitApp/MapKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppApp.swift
+//  MapKitApp
+//
+//  Created by 富澤明大 on 2024/08/04.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
